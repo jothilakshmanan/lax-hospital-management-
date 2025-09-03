f1a14fec361c51a4dec73283dde25ee99003651b
jothilakshmanan <jothilaxman7@gmail.com>
2025-09-03 21:58:52 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/lax-hospital-management-/', // <-- your repo name
+  // base: '/lax-hospital-management-/', // <-- your repo name
+  base: './',
   plugins: [react()],
 })
